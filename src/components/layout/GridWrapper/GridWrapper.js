4f8a573c191025439dbdb88d90aaf426bb63d332
余家豪 YU KAHAO <124207407+zo-glass@users.noreply.github.com>
2026-01-01 00:00:00 -0300

    Add responsivity

--- src/components/layout/GridWrapper/GridWrapper.js
+++ src/components/layout/GridWrapper/GridWrapper.js
@@ -1,11 +1,14 @@
 import styles from './GridWrapper.module.css'
 
-export default function GridWrapper({ children, columns }) {
+export default function GridWrapper({ children, columns, mobileColumns }) {
 	return (
 		<>
 			<div
 				className={styles.container}
-				style={{ '--columns': columns }}
+				style={{ 
+					'--columns': columns,
+					'--mobile-columns': mobileColumns
+				}}
 			>
 				{children}
 			</div>
