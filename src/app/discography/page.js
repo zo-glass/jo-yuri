37a3d38bbfba58e92f092b72b440cfbf0d9311fd
余家豪 YU KAHAO <124207407+zo-glass@users.noreply.github.com>
2026-01-01 00:00:00 -0300

    Add pagination

--- src/app/discography/page.js
+++ src/app/discography/page.js
@@ -1,14 +1,28 @@
 import styles from './page.module.css'
+import { notFound } from 'next/navigation'
 
 import { getData } from '@/common/apiService'
 
 import GridWrapper from '@/components/layout/GridWrapper/GridWrapper'
 import MusicCard from '@/components/cards/MusicCard/MusicCard'
+import Pagination from '@/components/ui/Pagination/Pagination'
 
-export default async function Discography() {
-    const res = await getData('discography', { limit: 4 })
+const ITENS_PER_PAGE = (2) * 2
+
+export default async function Discography({ searchParams }) {
+    const { page = 1 } = await searchParams
+    const offset = (page - 1) * ITENS_PER_PAGE
+
+    const res = await getData('discography', { limit: ITENS_PER_PAGE, offset: offset })
 
     const discographyItems = res?.items || []
+    discographyItems?.items?.sort((a, b) => b.createdAt - a.createdAt)
+
+    const totalPages = Math.ceil(res?.pageInfo?.totalResults / ITENS_PER_PAGE) || 1
+
+    if (discographyItems.length === 0) {
+        notFound()
+    }
 
     return (
         <>
@@ -18,6 +32,9 @@ export default async function Discography() {
                     <MusicCard key={index} item={item} />
                 ))}
             </GridWrapper>
+            <div className={styles.pagination}>
+                <Pagination totalPages={totalPages} />
+            </div>
         </>
     )
 }
