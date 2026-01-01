90a2f384eb8ffa470e6c0c9d099b747e67d93919
余家豪 YU KAHAO <124207407+zo-glass@users.noreply.github.com>
2026-01-01 00:00:00 -0300

    Add discography home

--- src/app/page.js
+++ src/app/page.js
@@ -3,17 +3,21 @@ import styles from "./page.module.css"
 import { getData } from '@/common/apiService'
 
 import Carousel from '@/components/pages/main/Carousel/Carousel'
+import MainDiscography from '@/components/pages/main/MainDiscography/MainDiscography'
 
 export default async function Home() {
-	const [carouselItems] = await Promise.all([
-		getData('carousel')
+	const [carouselItems, discographyItems] = await Promise.all([
+		getData('carousel'),
+		getData('discography', {limit: 4})
 	])
 
 	carouselItems?.items?.sort((a, b) => b.createdAt - a.createdAt)
+	discographyItems?.items?.sort((a, b) => b.createdAt - a.createdAt)
 
 	return (
 		<>
 			<Carousel items={carouselItems?.items || []} />
+			<MainDiscography items={discographyItems?.items || []} />
 		</>
 	)
 }
