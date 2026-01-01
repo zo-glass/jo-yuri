bdea1f11b399604b3d89ea37aba005379850818d
余家豪 YU KAHAO <124207407+zo-glass@users.noreply.github.com>
2026-01-01 00:00:00 -0300

    Add cursor pagination

--- src/app/discography/page.js
+++ src/app/discography/page.js
@@ -2,27 +2,45 @@ import styles from './page.module.css'
 import { notFound } from 'next/navigation'
 
 import { getData } from '@/common/apiService'
+import { create, read, clear } from '@/common/redisCache'
 
 import GridWrapper from '@/components/layout/GridWrapper/GridWrapper'
 import MusicCard from '@/components/cards/MusicCard/MusicCard'
 import Pagination from '@/components/ui/Pagination/Pagination'
 
-const ITENS_PER_PAGE = (2) * 2
+const ITEMS_PER_PAGE = (2) * 2
 
 export default async function Discography({ searchParams }) {
     const { page = 1 } = await searchParams
-    const offset = (page - 1) * ITENS_PER_PAGE
+    const offset = (page - 1) * ITEMS_PER_PAGE
+    let params = {}
+    let token = null
+
+    if (Number(page) === 1) {
+        params = { limit: ITEMS_PER_PAGE, offset: offset }
+    } else {
+        token = await read('discography', page)
+        if (token.token) {
+            params = { limit: ITEMS_PER_PAGE, pageToken: token.token }
+        } else {
+            params = { limit: ITEMS_PER_PAGE, offset: offset }
+        }
+    }
 
     const res = await getData('discography', {
-        params: { limit: ITENS_PER_PAGE, offset: offset },
+        params: params,
         revalidate: Number(process.env.REVALIDATE_TIME_LONG),
         tags: ['discography']
     })
 
-    const discographyItems = res?.items || []
-    discographyItems?.items?.sort((a, b) => b.createdAt - a.createdAt)
+    if (!token?.hasNext && res?.nextPageToken) {
+        await create('discography', String(Number(page)+1), res?.nextPageToken)
+    }
 
-    const totalPages = Math.ceil(res?.pageInfo?.totalResults / ITENS_PER_PAGE) || 1
+    const discographyItems = res?.items || []
+    discographyItems?.sort((a, b) => b.createdAt - a.createdAt)
+
+    const totalPages = Math.ceil(res?.pageInfo?.totalResults / ITEMS_PER_PAGE) || 1
 
     if (discographyItems.length === 0) {
         notFound()
