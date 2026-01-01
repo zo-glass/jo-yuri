37ce13330a87c867e2b85d399c2c93200255b1ae
余家豪 YU KAHAO <124207407+zo-glass@users.noreply.github.com>
2026-01-01 00:00:00 -0300

    Carousel configuration

--- src/components/pages/main/Carousel/Carousel.js
+++ src/components/pages/main/Carousel/Carousel.js
@@ -1,19 +1,29 @@
-"use client"
+'use client'
 
-import styles from "./Carousel.module.css"
+import styles from './Carousel.module.css'
 
-import { Swiper, SwiperSlide } from "swiper/react"
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Autoplay } from 'swiper/modules'
 import 'swiper/css'
-
 
 export default function Carousel({ items }) {
     return (
         <>
-            <Swiper className={styles.swiper}>
+            <Swiper
+                slidesPerView={1}
+                spaceBetween={0}
+                loop={true} 
+                modules={[ Autoplay ]}
+                autoplay={{
+                    delay: 5000,
+                    disableOnInteraction: false,
+                }}
+                className={styles.swiper}
+            >
                 {items?.map((i, index) => {
                     return (
                         <SwiperSlide key={index}>
-                            <a href={i.href} target="_blank" rel="noopener noreferrer">
+                            <a href={i.href} target='_blank' rel='noopener noreferrer'>
                                 <img src={i.src} alt={i.alt} className={styles.image} />
                             </a>
                         </SwiperSlide>
